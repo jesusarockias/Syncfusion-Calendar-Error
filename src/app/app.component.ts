8aa63891491456be063bf79d6ba0e80f0bff1b53
jesusarockias@syncfusion.com <jesusarockias@syncfusion.com>
2020-09-23 17:22:36 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: `
+    <!-- To Render Calendar -->
+    <ejs-calendar></ejs-calendar>`
+})
+export class AppComponent {
+    constructor() {
+    }
+}
